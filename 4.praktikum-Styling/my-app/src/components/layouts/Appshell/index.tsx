8e8d9b220d225b8659aad2c3a0c3acf1f4428fdf
DimasWW <dimas.w@polinema.ac.id>
2026-02-07 13:50:50 +0700

    dirubah numbering foldernya

---
+++ 4.praktikum-Styling/my-app/src/components/layouts/Appshell/index.tsx
@@ -0,0 +1,42 @@
+import Navbar from "../navbar";
+
+
+/*
+Mendefinisikan tipe props menggunakan TypeScript
+children:
+ - Properti bawaan React
+ - Bertipe React.ReactNode
+ - Berisi elemen JSX yang dibungkus oleh AppShell
+*/
+type AppShellProps = {
+    children: React.ReactNode;
+}
+
+/*
+const AppShell = (props:AppShellProps) => {
+  - Membuat functional component
+  - Menerima props bertipe AppShellProps
+  - Menggunakan arrow function
+*/
+const AppShell = (props:AppShellProps) => {
+  
+  /*
+  const { children } = props;
+  - Mengambil nilai children dari props
+  - Memudahkan pemanggilan tanpa menulis props.children
+  */
+  const { children } = props;
+  return (
+  <main>
+    <Navbar />
+    {children}
+ 
+  </main>
+
+    );
+};
+
+/* 
+    {children} → konten halaman yang berubah-ubah */
+export default AppShell;
+
