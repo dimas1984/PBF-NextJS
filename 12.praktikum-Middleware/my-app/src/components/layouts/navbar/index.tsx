e47a1c3215e0bb88756446e03d70dc087a28a8f5
DimasWW <dimas.w@polinema.ac.id>
2026-02-15 18:14:33 +0700

    middleware done

---
+++ 12.praktikum-Middleware/my-app/src/components/layouts/navbar/index.tsx
@@ -0,0 +1,15 @@
+import syles from './navbar.module.css';
+
+
+const Navbar = () => {
+  return (
+  <div className={syles.navbar}>
+    <div className="big">
+      Navbar
+    </div> 
+ </div>
+    );
+};
+
+export default Navbar;
+
