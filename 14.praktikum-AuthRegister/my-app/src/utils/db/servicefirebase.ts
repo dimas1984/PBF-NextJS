b0ac8da73bb57f0d1a41e87fad2a244032d08b4b
DimasWW <dimas.w@polinema.ac.id>
2026-02-16 21:41:29 +0700

    masih salah di user already seharusnya email

---
+++ 14.praktikum-AuthRegister/my-app/src/utils/db/servicefirebase.ts
@@ -0,0 +1,71 @@
+import {
+  getFirestore,collection,
+  getDocs,Firestore,getDoc,doc,
+  query,addDoc,where,
+} from "firebase/firestore";
+import app from "./firebase";
+import bcrypt from "bcrypt";
+
+const db = getFirestore(app);
+
+export async function retrieveProducts(collectionName: string) {
+  const snapshot = await getDocs(collection(db, collectionName));
+  const data = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  return data;
+}
+
+export async function retrieveDataByID(collectionName: string, id: string) {
+  const snapshot = await getDoc(doc(db, collectionName, id));
+  const data = snapshot.data();
+  return data;
+}
+
+export async function signUp(
+  userData: {
+    email: string;
+    fullname: string;
+    password: string;
+    role?: string;
+  },
+  callback: Function,
+) {
+  const q = query(
+    collection(db, "users"),
+    where("email", "==", userData.email),
+  );
+  const querySnapshot = await getDocs(q);
+  const data = querySnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  // console.log("Query result:", data);
+
+  if (data.length > 0) {
+    // user belum ada → boleh daftar
+    // await addDoc(collection(db, "users"), userData);
+    // console.log("User registered:", data);
+    callback({
+      status: "error",
+      message: "Email already exists",
+    });
+  } else {
+    userData.password = await bcrypt.hash(userData.password, 10);
+    userData.role = "user";
+    await addDoc(collection(db, "users"), userData)
+      .then(() => {
+        callback({
+          status: "success",
+          message: "Email registered successfully",
+        });
+      })
+      .catch((error) => {
+        callback({
+          status: "error",
+          message: error.message,
+        });
+      });
+  }
+}
