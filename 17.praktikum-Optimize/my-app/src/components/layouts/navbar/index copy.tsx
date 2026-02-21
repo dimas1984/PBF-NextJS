33c556eb997d7c69c56625cf9018c4050d751ac6
DimasWW <dimas.w@polinema.ac.id>
2026-02-21 21:16:07 +0700

    optimaze done

---
+++ 17.praktikum-Optimize/my-app/src/components/layouts/navbar/index copy.tsx
@@ -0,0 +1,34 @@
+import syles from './navbar.module.css';
+import { signIn, signOut, useSession } from "next-auth/react"
+
+const Navbar = () => {
+  const {data}:any = useSession()
+  //const { data: session } = useSession()
+  // console.log("session", session)
+  return (
+  <div className={syles.navbar}>
+    <div className="big">
+      Navbar
+    </div> 
+    <div>
+      {data ? (
+        <div>
+          <p>Welcome, {data.user.fullname}</p>
+        </div>
+      ) : (
+        <div>
+          <p>Not signed in</p>
+        </div>
+      )}
+    </div>
+    {data ? (
+      <button onClick={() => signOut()}>Sign Out</button>
+    ) : (
+      <button onClick={() => signIn()}>Sign In</button>
+    )}
+       </div>
+    );
+};
+
+export default Navbar;
+
