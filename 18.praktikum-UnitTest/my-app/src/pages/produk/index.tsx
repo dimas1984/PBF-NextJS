9e909b0e6127d3d887943220462a9c97dc1c476a
DimasWW <dimas.w@polinema.ac.id>
2026-02-22 09:01:39 +0700

    unit testing jest done

--- 18.praktikum-UnitTest/my-app/src/pages/produk/index.tsx
+++ 18.praktikum-UnitTest/my-app/src/pages/produk/index.tsx
@@ -11,24 +11,7 @@ const kategori = () => {
   // const [isLogin, setIsLogin] = useState(true);
   const { push } = useRouter();
   const [products, setProducts] = useState([]);
-  // console.log("products:", products);
-  // useEffect(() => {
-  //   if (!isLogin) {
-  //     push("/auth/login");
-  //   }
-  //   },[]);
-
-  // useEffect(() => {
-  //   fetch("/api/produk")
-  //     .then((response) => response.json())
-  //     .then((responsedata) => {
-  //       setProducts(responsedata.data);
-  //       // console.log("Data produk:", responsedata.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching produk:", error);
-  //     });
-  // }, []);
+  
   
   const { data, error,isLoading } = useSWR("/api/produk", fetcher);
   //cek apakah data, error, dan isLoading sudah benar
@@ -39,7 +22,7 @@ const kategori = () => {
 
   return (
     <div>
-      <TampilanProduk products={isLoading ? [] : data.data} />
+      <TampilanProduk products={isLoading ? [] : data?.data} />
     </div>
   );
 };
